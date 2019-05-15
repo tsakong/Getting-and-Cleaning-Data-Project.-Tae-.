f543a6d75431c427a43b35ab143e820580e72729
tsakong <40179317+tsakong@users.noreply.github.com>
2019-05-15 16:06:38 -0700

    Update and rename R.scripts to run_Analysis.R

---
+++ run_Analysis.R
@@ -0,0 +1,6 @@
+# You should create one R script called run_analysis.R that does the following.
+### 1. Merges the training and test sets to create one data set.
+### 2. Extracts ony the measurements on the mean and standard deviation for each measurement.
+### 3. Uses descriptive activity names to name the activities in the data set.
+### 4. Appropriately labels the data set with descriptive data set with descriptive variable names.
+### From the data set in setp 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
