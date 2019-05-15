17fd9fbad40e3c98dcb1d7c0c2c2382e9378d9f5
tsakong <40179317+tsakong@users.noreply.github.com>
2019-05-15 16:07:31 -0700

    Update run_Analysis.R

--- run_Analysis.R
+++ run_Analysis.R
@@ -3,4 +3,4 @@
 ### 2. Extracts ony the measurements on the mean and standard deviation for each measurement.
 ### 3. Uses descriptive activity names to name the activities in the data set.
 ### 4. Appropriately labels the data set with descriptive data set with descriptive variable names.
-### From the data set in setp 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+### 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
